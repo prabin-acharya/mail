f1ddb376bc067be3dbd13d688f72a54200b1be87
prabin-acharya@ <acharyaprabin101@gmail.com>
2021-06-12 23:48:41 +0545

    read,archive,build_email/emails implemented P.S. Archive button doesn't work as intended

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -59,42 +59,46 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  
-
   //Get data of the corresponding mailbox from the server
   fetch(`emails/${mailbox}`)
   .then((response)=>response.json())
   .then((emails)=>{
     emails.forEach(function(email){
     const parent_div = document.createElement('div') 
-    parent_div.addEventListener('click', ()=>read_email(email.id))
     build_emails(email, parent_div, mailbox)
+    parent_div.addEventListener('click', ()=>read_email(email.id))
+    document.querySelector('#emails-view').appendChild(parent_div);
   })
   })
   .catch((error)=>console.log(error))
 
 
 function build_emails(email, parent_div, mailbox){
+  const content = document.createElement("div")
+  const recipients = document.createElement("strong")
   if(mailbox==='inbox'){
-    if(email.read===false){
-      parent_div.innerHTML = ` <b>${email.sender}</b> ${email.subject} `
-    }else{
-      parent_div.innerHTML = ` ${email.sender} :   ${email.subject} `
-    }
-  }else if(mailbox==='sent'){
-    parent_div.innerHTML = ` ${email.recipients} : ${email.subject} `
-  }else{
-    parent_div.innerHTML = ` ${email.recipients} :  ${email.subject} `
+    recipients.innerHTML = ` ${email.sender} : `
+  }else {
+    recipients.innerHTML = ` ${email.recipients} : `
   }
+  content.appendChild(recipients);
+  content.innerHTML += email["subject"];
 
   const date = document.createElement("div");
   date.innerHTML = email["timestamp"];
   date.style.display = "inline-block";
   date.style["font-size"] = '14px';
   date.style.float = "right";
-  parent_div.appendChild(date);
 
-  document.querySelector('#emails-view').appendChild(parent_div);
+  if(email['read']){
+    parent_div.className = "text-muted";
+    date.className = "text-muted";
+  }else{
+    date.style.color = 'black';
+  }
+  content.appendChild(date);
+
+  parent_div.appendChild(content);
 
   parent_div.style.borderStyle = "ridge";
   parent_div.style.borderWidth = "3px";
@@ -105,47 +109,82 @@ function build_emails(email, parent_div, mailbox){
 }
 
 
-
 function read_email(email_id){
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#email-view').style.display = 'block';
 
   // Erase any email that was here
   document.querySelector("#email-view").innerHTML = "";
+
   fetch(`emails/${email_id}`)
   .then((response)=>response.json())
   .then((email)=>{
-    let display = `
-      <h3>${email.subject}</h3>
-      From : <b>${email.sender}</b>
-      At : ${email.timestamp}
-      <br><br>
-      ${email.body}
-      <br>
-      <button id='archivebutton'>Archive this Email</button>
-    `;
-    document.querySelector('#email-view').innerHTML = display;
-    document.querySelector('#archivebutton').addEventListener('click',()=> {
-      archive_email(email)
-      load_mailbox('inbox')
+    build_email(email);
+  })
+  .catch(error => console.log(error));
+  fetch(`/emails/${email_id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      read: true
     })
-  })
-  fetch(`/emails/${email_id}`,{
-    method:"PUT",
-    body:JSON.stringify({
-      read : true
-      })
-  })
+  });
 }
+
+
+function build_email(email){
+  const from = document.createElement('div')
+  const to = document.createElement('div')
+  const subject = document.createElement('div')
+  const timestamp = document.createElement('div')
+  const body = document.createElement('div')
+  const reply_button = document.createElement('div')
+  const archive_button = document.createElement('div')
+
+  from.innerHTML = `<strong> From : </strong> ${email.sender}`;
+  to.innerHTML = `<strong> To : </strong> ${email.recipients}`;
+  subject.innerHTML = `<strong> Subject : ${email.subject} </strong>`;
+  timestamp.innerHTML = `<strong> Timestamp : </strong> ${email.timestamp}`
+  body.innerHTML = email.body;
+
+  archive_button.innerHTML = '';
+  if (email["archived"]) {
+    archive_button.innerHTML += "Unarchive";
+  } else {
+    archive_button.innerHTML += "Archive";
+  }
+  archive_button.classList = "btn btn-outline-primary m-2";
+  archive_button.addEventListener("click", () => {
+    if(archive_button.innerHTML === "Archive") archive_button.innerHTML === "Unarchive"
+    else archive_button.innerHTML === "Archive"
+    archive_email(email);
+
+  });
+
+  reply_button.innerHTML = 'Reply';
+  reply_button.classList = "btn btn-outline-primary m-2";
+  reply_button.addEventListener("click", () => compose_reply(email));
+
+  document.querySelector("#email-view").appendChild(from);
+  document.querySelector("#email-view").appendChild(to);
+  document.querySelector("#email-view").appendChild(subject);
+  document.querySelector("#email-view").appendChild(timestamp);
+  document.querySelector("#email-view").appendChild(archive_button);
+  document.querySelector("#email-view").appendChild(reply_button);
+  document.querySelector("#email-view").appendChild(document.createElement("hr"));
+  document.querySelector("#email-view").appendChild(body);
+}
+
 
 function archive_email(email){
   fetch(`/emails/${email.id}`,{
     method:"PUT",
     body:JSON.stringify({
-      archived : true
-    }),
+      archived : !email["archived"]
+    })
   })
   .catch((error) => console.log(`Hello:${error}`))
-  }
 }
+
+}
+
 
